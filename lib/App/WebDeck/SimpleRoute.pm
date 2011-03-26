722f0433a6d5e34cb9cf26e3256e6747798edc52
Brock Wilcox <awwaiid@thelackthereof.org>
2011-03-26 17:16:19 -0400

    Add documentation for routing module

--- lib/App/WebDeck/SimpleRoute.pm
+++ lib/App/WebDeck/SimpleRoute.pm
@@ -2,7 +2,7 @@ package App::WebDeck::SimpleRoute;
 
 =head1 NAME
 
-SimpleRoute - Minimalist path matching and routing
+App::WebDeck::SimpleRoute - Minimalist path matching and routing
 
 =head1 SYNOPSIS
 
@@ -16,7 +16,7 @@ SimpleRoute - Minimalist path matching and routing
 
     '/comment'           => sub { say "Adding a comment..." },
 
-    # Now let's get fancy
+    # Now let's get fancy. Params are in $_, @_, and %+, take your pick.
     '/blog/:year/:month' => [ year => qr/\d{4}/, month => qr/\d{2}/ ] => sub {
       say "Blog posts from $+{year}/$+{month}";
     },
@@ -31,6 +31,35 @@ use strict;
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw( route );
+
+=head1 EXPORTS
+
+=head2 route $path => [ ... rules ... ];
+
+This is the only sub, and the only export. It takes two parameters -- the path that we're going to match against, and a list of rules to try.
+
+Each rule consists of at least two parts -- a pattern, and a callback. You may optionally include an arrayref of regex validators as well.
+
+Patterns are basically paths, but you can add named captures. Internally these are turned into plain old regexes that we match against.
+
+Some examples:
+
+  # Just a path and a callback
+  '/comment' => sub { say "Adding a comment..." },
+
+  # A fancier path, which has a parameter
+  '/post/:postname' => sub { say "You want $_->{postname}, eh?" },
+
+  # Super fancy -- path with params, regex validation, and the callback
+  '/blog/:year/:month'
+    => [ year => qr/\d{4}/, month => qr/\d{2}/ ]
+    => sub { say "Blog posts from $+{year}/$+{month}" },
+
+The named parameters can be gotten at 3 ways. Most normal-like is that they are passed through @_, like C<< callback(year => '2011', month => '02') >>. Next is that a hashref of the commands is put into $_. Finally, since we do a by-name regex match, you can get to them through %+, like C<< $+{year} >>.
+
+Since we are really just turning the pattern into a regex, you can do any sort of regex type stuff as well. You could even ignore the :name syntax altogether if you want.
+
+=cut
 
 sub route {
   my $path = shift;
@@ -59,6 +88,22 @@ sub route {
     }
   }
 }
+
+=head1 SEE ALSO
+
+There are a bunch of modules just like this one but different. Miyagawa has gathered a lovely list of ones that can be used with Plack over at L< https://github.com/miyagawa/plack-dispatching-samples >. Notably, the closest to this interface is the one provided by L<Mojolicious::Lite>.
+
+=head1 AUTHOR
+
+  Brock Wilcox <awwaiid@thelackthereof.org> - http://thelackthereof.org/
+
+=head1 COPYRIGHT
+
+  Copyright (c) 2011 Brock Wilcox <awwaiid@thelackthereof.org>. All rights
+  reserved. This program is free software; you can redistribute it and/or
+  modify it under the same terms as Perl itself.
+
+=cut
 
 1;
 
